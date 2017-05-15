5b74efbe78b1bda7054dd21b15e6520483b606a6
wesleywmd <therealwesleywmd@gmail.com>
2017-05-14 23:41:21 -0400

    add help messages to invent:setup:sql

--- src/Invent/Commands/SetupSqlCommand.php
+++ src/Invent/Commands/SetupSqlCommand.php
@@ -7,21 +7,32 @@ use Invent\FileIO;
 use Invent\Service\Version;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SetupSqlCommand extends AbstractModuleCommand
 {
     const COMMAND_NAME = "invent:setup:sql";
-    const ARGUMENT_HELPER_HANDLE = "helper_handle";
+    const OPTION_RESOURCE = "resource";
 
     protected $moduleMustExist = true;
+
+    private $help = <<<HELP
+Creates an install script if no install script is found, otherwise it creates an upgrade script. The config xml will 
+have its resources registered properly and the version will be increased.
+HELP;
 
     protected function configure()
     {
         $this->setName(self::COMMAND_NAME)
-            ->setDescription("")
-            ->setHelp("")
-            ->addModuleInputs();
+            ->setDescription("Creates an install script or upgrade script")
+            ->setHelp($this->help)
+            ->addModuleInputs()
+            ->addOption(
+                self::OPTION_RESOURCE, "r",
+                InputOption::VALUE_REQUIRED,
+                "Type of setup resource to create"
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
